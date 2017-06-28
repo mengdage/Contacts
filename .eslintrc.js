3b3c3f125f16d31d43288c9f35f7229bad2e2687
meng <wulalameng@gmail.com>
2017-06-28 15:30:03 -0400

    add starter files

--- .eslintrc.js
+++ .eslintrc.js
@@ -237,7 +237,10 @@ module.exports = {
         "prefer-rest-params": "error",
         "prefer-spread": "error",
         "prefer-template": "error",
-        "quote-props": "error",
+        "quote-props": [
+          "error",
+          "as-needed"
+        ],
         "quotes": [
             "error",
             "single"
