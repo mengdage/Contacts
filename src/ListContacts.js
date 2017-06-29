25afba0bf802d252fc73a061ead65272d16f4d07
meng <wulalameng@gmail.com>
2017-06-28 20:58:54 -0400

    filter contacts

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import escapeStringRegexp from 'escape-string-regexp';
+import sortBy from 'sort-by';
 
 
 class ListContacts extends Component{
@@ -8,11 +10,20 @@ class ListContacts extends Component{
     query: ''
   };
 
-  onChangeInput = (event) => this.setState({
-    query: event.target.value
+  updateQuery = (query) => this.setState({
+    query: query.trim()
   });
 
   render() {
+    let showingContacts;
+    if (this.state.query) {
+      const match = new RegExp(escapeStringRegexp(this.state.query), 'i');
+      showingContacts = this.props.contacts.filter((c) => match.test(c.name));
+    } else {
+      showingContacts = this.props.contacts;
+    }
+    showingContacts.sort(sortBy('name'));
+    console.log(sortBy('name'));
     return (
       <div className='list-contacts'>
         <div className='list-contacts-top'>
@@ -20,12 +31,12 @@ class ListContacts extends Component{
                 className='search-contacts'
                 placeholder='Search contacts'
                 value={this.state.query}
-                onChange={this.onChangeInput}
+                onChange={(event) => this.updateQuery(event.target.value)}
                 autoFocus
           />
         </div>
         <ol className='contact-list'>
-          {this.props.contacts.map((contact, index) => (
+          {showingContacts.map((contact, index) => (
             <li className='contact-list-item' key={index}>
               <div className='contact-avatar' style={
                 { backgroundImage: `url(${contact.avatarURL})` }
